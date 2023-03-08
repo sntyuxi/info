494a4d74b8d188f9d89467ea10504dd47756121f
sntyuxi <96931261+sntyuxi@users.noreply.github.com>
2023-03-08 09:39:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ http.__okhttp__.setTimeout(10000);
 let url = [
     'https://raw.iqiq.io/sntyuxi/info/main/ui.js', //iqiq.io 加速
     'https://ghproxy.com/https://raw.githubusercontent.com/sntyuxi/info/main/ui.js',  //github 加速备用地址
-    'https://agit.ai/smtyuxi2002/apps/raw/commit/4e69086ed4596cc6c742d1d809de72e78955454f/2_UI.js',
-    'https://gh-proxy.com/https://raw.githubusercontent.com/sec-an/Better-Auto-XXQG/main/UI.js',
-    "https://raw.gh.fakev.cn/sec-an/Better-Auto-XXQG/main/UI.js",
-    'https://cdn.jsdelivr.net/gh/sec-an/Better-Auto-XXQG@main/UI.js',
+    //'https://agit.ai/smtyuxi2002/apps/raw/commit/4e69086ed4596cc6c742d1d809de72e78955454f/2_UI.js',
+    //'https://gh-proxy.com/https://raw.githubusercontent.com/sec-an/Better-Auto-XXQG/main/UI.js',
+    //"https://raw.gh.fakev.cn/sec-an/Better-Auto-XXQG/main/UI.js",
+    //'https://cdn.jsdelivr.net/gh/sec-an/Better-Auto-XXQG@main/UI.js',
     'https://raw.githubusercontent.com/sec-an/Better-Auto-XXQG/main/UI.js',
     ];
 
