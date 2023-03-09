bbe99bd410b87c81d7684259a9be3ca652d673c3
sntyuxi <96931261+sntyuxi@users.noreply.github.com>
2023-03-09 22:07:43 +0800

    Update 0.js

--- 0.js
+++ 0.js
@@ -170,7 +170,7 @@ threads.start(function () {
   toastLog("开始自动获取截图权限");
   var btn = className("android.widget.Button").textMatches(/允许|立即开始|START NOW/).findOne(5000);
   if (btn) {
-    sleep(1000);
+    sleep(5000);
     btn.click();
   }
   toastLog("结束获取截图权限");
