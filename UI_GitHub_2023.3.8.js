38929e49240abc5c69ebc313c301315eb090e76b
sntyuxi <96931261+sntyuxi@users.noreply.github.com>
2023-04-20 09:17:31 +0800

    Update UI_GitHub_2023.3.8.js

--- UI_GitHub_2023.3.8.js
+++ UI_GitHub_2023.3.8.js
@@ -14,7 +14,7 @@ ui.layout(
     <drawer id="drawer">
         <vertical>
             <appbar>
-                <toolbar id="toolbar" bg="#ff4fb3ff" title="GitHub 學習助手"/>
+                <toolbar id="toolbar" bg="#ff4fb3ff" title="GitHub 新學習助手"/>
                 <tabs id="tabs" bg="#ff4fb3ff"/>
             </appbar>
             <viewpager id="viewpager">
